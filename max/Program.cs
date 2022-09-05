8cd30437dac5237dcb61d913b18f288dd10b4fc0
Roman <levenets_r@mail.ru>
2022-09-05 18:09:56 +0600

    prm1

---
+++ max/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введи 2 числа и мы тебе скажем какое из них больше");
+            int a, b;
+            Console.WriteLine("Введи свое первое число - ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введи свое второе число - ");
+            b = Convert.ToInt32(Console.ReadLine());
+ 
+            bool cht1 = a > b;
+            bool cht2 = a == b;
+            bool cht3 = a < b;
+ 
+            if (cht1)
+            {
+                Console.WriteLine($"Число {a} больше чем {b}");
+            }
+            else if (cht2)
+            {
+                Console.WriteLine($"Числа {a} и {b} равны");
+            }
+            else
+            {
+                Console.WriteLine($"Число {b} больше чем {a}");
+            }
