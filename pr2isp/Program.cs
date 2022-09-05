59b0590dd838243094b1425565e7e0903bb161cb
Roman <levenets_r@mail.ru>
2022-09-05 18:41:43 +0600

    pr2isp

---
+++ pr2isp/Program.cs
@@ -0,0 +1,9 @@
+﻿double a = 3, b = 5, c = 10;
+ if (a > b && a > c)
+                    Console.WriteLine("Nubmer Max = " + a); 
+                else if (b > c)
+                    Console.WriteLine("Number Max = " + b); 
+                else
+                    Console.WriteLine("Number Max = " + c); 
+            
+            Console.ReadLine();
