a28c30823d66b73f79896cb88f59b1c10314bbef
Roman <levenets_r@mail.ru>
2022-09-06 11:58:37 +0600

    pr5

---
+++ pr5/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введи трёхзначное число: ");
+        int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+        string stringNumber = Convert.ToString(threeDigitNumber);
+        Console.WriteLine("вторая цифра этого числа -> " + stringNumber[1]);
+    }
+}
