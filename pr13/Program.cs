4d4f61b7ea226b7021ab4236996c0ad7a21b6068
Roman <levenets_r@mail.ru>
2022-09-17 10:37:28 +0600

    pr13

---
+++ pr13/Program.cs
@@ -0,0 +1,10 @@
+﻿             int[] array = new int[8];
+            Random myRandom = new Random();
+ 
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = myRandom.Next(0, 8);
+                Console.Write("{0} ", array[i]);
+            }
+ 
