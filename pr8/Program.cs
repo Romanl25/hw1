29c87821f4438053bb30b7e66cf8029586e2eab7
Roman <levenets_r@mail.ru>
2022-09-17 10:04:51 +0600

    pr9+

--- pr8/Program.cs
+++ pr8/Program.cs
@@ -2,7 +2,7 @@
 string number = Console.ReadLine();
 
 {
-  if (number[0]==number[4] || number[1]==number[3]){
+  if (number[0]==number[4] && number[1]==number[3]){
     Console.WriteLine($"Ваше число: {number} - палиндром.");
   }
   else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
